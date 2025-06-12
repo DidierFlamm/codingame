cea8a42911218c1f0b12dd81cfcbc660ea71d1aa
Didier Flamm <137876296+DidierFlamm@users.noreply.github.com>
2025-06-12 02:32:20 +0200

    resistance: cleaned code and add 3 smart solutions from the board

--- Puzzles/Expert/La résistance.py
+++ Puzzles/Expert/La résistance.py
@@ -1,6 +1,13 @@
 """
 https://www.codingame.com/training/expert/the-resistance
 
+Opportunités d'apprentissage
+
+Mémoïsation
+Programmation dynamique
+Récursion
+Encodage
+
 Objectif
 Vous travaillez au musée de la Résistance nationale et vous venez d'exhumer des centaines de documents contenant des transmissions codées en Morse. Dans les documents, aucun espace n'a été retranscrit pour séparer les lettres et les mots qui se cachent derrière une séquence en Morse. Une séquence décodée peut donc avoir différentes interprétations.
 
@@ -14,6 +21,7 @@ Le Morse est un codage composé de points et de traits représentant des lettres
  Q  --.-	 R  .-.	 S  ...	 T  -
  U  ..-	 V  ...-	 W  .--	 X  -..-
  Y  -.--	 Z  --..
+
 Puisqu'aucun des espaces n'ont été retranscrits, plusieurs interprétations des messages sont possibles. Par exemple, la séquence -....--.-. peut aussi bien correspondre à BAC, BANN, DUC, DU TETE, ....
 
 Un être humain est capable de reconnaître le découpage adéquat grâce à sa connaissance de la langue mais pour une machine c'est plus délicat. Pour que votre programme puisse faire l'équivalent vous avez à votre disposition un dictionnaire contenant un ensemble de mots corrects.
@@ -59,152 +67,18 @@ Sortie
 2
 """
 
-"""
-Tips pour récursif ! 
-string[position:].startswith(short_string)
-takes 10x more time then:
-
-string.startswith(short_string, position)
-
-"""
-
-""" 
-TRIE data structure
-https://en.wikipedia.org/wiki/Trie
-"""
-
-""" 
-I used DP using a trie as data store.
-"""
-
-""" 
-At the end storing where each letter begins in the input string helped me for faster finding where each word from the dictionary begins. After that I used a recursion over the dictionary words and count how many words start at a given index of the input word and span to the end of the input string, that way I do not go through the how string every time.
-"""
-""" 
-TIPS 1
-Let’s say you’ve got a sequence of 1000 dots and dashes. One possible word ends at position 100, but other words are still possible on the same path (e.g. you’ve parsed out the word HELLO the words HELLO and HELLOWORLD are both in the dictionary). This is a fork; from position 100 you can start a new word (path B) or continue checking to see if HELLOWORLD is possible (path A).
-
-Paths A and B both end somewhere. Let’s say they both end at the same spot, position 150 (e.g. the word WORLD is also in the dictionary). This is what I mean by the path reconverging; paths A and B both lead to new path C at position 150. When you get done parsing path A, you parse path C until you hit an endpoint, then go back to parse path B. Path B ends at the beginning of path C. However, since you already parsed path C once, you don’t need to do it again.
-
-This kind of thing happens a lot in the larger tests, and the paths can split several times before recombining. You can waste a lot of compute cycles parsing out stuff you already did if you don’t take it into account.
-"""
-
-""" 
-TIPS 2
-Switching to a decent cache allowed me to use the simplest recursion and implied DFS, with the DP boosting performance to millisecond range
-
-"""
-
-""" 
-TIPS 3
-~34 ms in Python3 for the last test case. struggled a lot until i discovered identical sequences could be different words… simple recursion with memoization.
-"""
-
-""" 
-TIPS 4
-A few words on this one. I did a standard recursion with strings matching & memoization to avoid recursion on already-done paths.
-
-"""
-
-""" 
-I used tries, as others did.
-Some have mentioned that memoization was not necessary; but the way I did it, after the trie was built (based on all the words in the dictionary), I stepped through the message exactly once. For each bit in the message, I checked whether the current node in the trie had a branch for that bit. If it did, and if the next node had leaves in the trie, I multiplied the number of leaves by the previous leaves and spawned a new “thread” starting from the base of the trie on the next bit. Well, actually, for each bit, I totalled the number of leaves occurring from all the relevant nodes, and created one new search thread with that number. And if the node represented in that thread had a NULL pointer for that branch, I took the thread out of the list.
-When I reached the last bit, instead of starting a new thread with that total number of leaves, I simply printed it.
-I didn’t run out of time trying to spawn new threads each time I found trie leaves, but I ran out of space, no matter how I did it. So, keeping track of all the leaves at a certain node of the trie AND summing them all up for each bit before spawning a new thread were forms of memoization - I kept track of how many words ended on that bit, rather than checking each one.
-I used C! I had two structs, one for the nodes of the trie and one for the search threads.
-I used linked lists for both, to make it easy to insert and delete. There was no disadvantage to using a linked list for the search threads because I had to step through each one anyway for each bit in the message.
-That was fun!
-
-"""
-
-test = 6
-
-# code pour tester le script hors site
-
-
-if test == 1:  # 1 lettre
-
-    inputs = [
-        "..-",
-        "6",
-        "A",
-        "B",
-        "C",
-        "HELLO",
-        "K",
-        "U",
-        "WORLD",
-    ]
-
-    output = 1
-
-
-"""
-# test forum
-
-# inputs = [".", "2", "E", "EEE"]
-
-# output = 1
-
-# test forum
-
-# inputs = ["....----", "4", "E", "EE", "T", "TT"]
-# output = 25
-
-# test forum
-
-# inputs = ["...----", "4", "E", "EE", "T", "TT"]
-# output = 15
-"""
-
-if test == 2:  # Détection correcte d'un mot
-    inputs = ["--.-------..", "5", "GOD", "GOOD", "MORNING", "G", "HELLO"]
-    output = 1
-
-
-if test == 3:  # simples messages
-    inputs = [
-        "......-...-..---.-----.-..-..-..",
-        "5",
-        "HELL",
-        "HELLO",
-        "OWORLD",
-        "WORLD",
-        "TEST",
-    ]
-
-    output = 2
-
-
-# test 4
-
-# test 5
-if test == 5:
-    inputs = ["-.-..---.-..---.-..--", "5", "CAT", "KIM", "TEXT", "TREM", "CEM"]
-    output = 125
-
-
-# test 6 : LC,LD
-
-if test == 6:
-
-    inputs = ["..............................................", "2", "E", "I"]
-    output = 2971215073
-"""
 inputs = [
-    "-....--.-.-....--.-.",
-    "3",
-    "BAC",
-    "BANN",
-    "DUTETE",
+    "......-...-..---.-----.-..-..-..",
+    "5",
+    "HELL",
+    "HELLO",
+    "OWORLD",
+    "WORLD",
+    "TEST",
 ]
 
-output = 9
-"""
-# test 6 custom
-if test == 6.1:
-    inputs = [".............................", "2", "E", "I"]
-    output = 832040
+# output = 2
+
 
 generator = (input for input in inputs)  # type: ignore
 
@@ -223,18 +97,15 @@ from collections import Counter  # noqa: E402
 
 start_time = time()
 
-DEBUG_MODE = 1
+DEBUG_MODE = 1  # 1 to view debug messages returned by eprint function, else 0
 
 
 def eprint(*args, **kwargs):
-    if DEBUG_MODE:
-        try:
+    try:
+        if DEBUG_MODE:
             print(*args, file=stderr, flush=True, **kwargs)
-        except Exception as e:
-            print("[EPRINT ERROR] Failed to print debug message.", file=stderr)
-            print("Args:", args, file=stderr)
-            print("Kwargs:", kwargs, file=stderr)
-            print("Exception:", e, file=stderr)
+    except Exception as e:
+        print("[EPRINT ERROR]", e, file=stderr, flush=True)
 
 
 morse = {
@@ -340,5 +211,108 @@ def DP_recursive_decode(index: int) -> int:
 result = DP_recursive_decode(0)
 
 print(result)
-eprint(result == output)  # type: ignore
+
 eprint("duration=", time() - start_time)
+try:
+    eprint(result == output)  # type: ignore
+except Exception as e:
+    print("[FINAL ERROR]", e, file=stderr, flush=True)
+
+
+# best solutions:
+
+# 1.memoization could be done without @lru_cache with a list like
+# count = [0] * (sequence_length+1)
+# count[0] = 1
+# and computing iteratively from this list, index by index, the number of messages.
+
+"""
+morse = {
+'A': '.-',  'B': '-...','C': '-.-.','D': '-..', 'E': '.',  'F': '..-.','G': '--.', 'H': '....','I': '..',
+'J': '.---','K': '-.-', 'L': '.-..','M': '--',  'N': '-.', 'O': '---', 'P': '.--.','Q': '--.-','R': '.-.',
+'S': '...', 'T': '-',   'U': '..-', 'V': '...-','W': '.--','X': '-..-','Y': '-.--','Z': '--..'}
+
+message = input()
+words = ["".join(morse[letter] for letter in input()) for _ in range(int(input()))]
+    
+counter = [0]*(len(message)+1)
+counter[0]=1
+
+for position, count in enumerate(counter):
+    if count:
+        for word in words:
+            if message.startswith(word, position):
+                counter[position + len(word)] += count
+                
+print(counter[-1])
+"""
+
+"""
+import sys
+import math
+
+code = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+     	'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..'
+}
+
+morsetext = input()
+n = int(input())
+morselen = len(morsetext)
+
+#create morse wordlist (list)
+wordlist = [""] * n
+for i in range(n):
+    word=input()
+    for char in word:
+        wordlist[i] += code[char]
+
+wordset=set(wordlist)  #much faster to search, but doesn't contain duplicates
+
+maxlen = len(max(wordset, key=len)) 
+minlen = len(min(wordset, key=len)) 
+
+#memoization
+count = [0] * (morselen+1)
+count[0] = 1
+
+for end in range(1,morselen+1):
+    for seglength in range(minlen, min(maxlen,end)+1 ):
+        segment = morsetext[end-seglength:end]
+        if segment in wordset :
+            count[end] += count[end-seglength] * wordlist.count(segment)
+
+print(count[-1]) 
+"""
+
+# the recursive function could use 'remainingcode' instead of index (performance to be checked...)
+""" 
+import functools
+import sys
+sys.setrecursionlimit(4000)
+morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'}
+
+code = input()
+
+ciphers = []
+n = int(input())
+for _ in range(n):
+    word = input()
+    ciphers.append(''.join([morse[letter] for letter in word]))
+
+@functools.lru_cache()
+def hunt(remainingcode):
+    if len(remainingcode) == 0:
+        return 1
+    if not any(remainingcode.startswith(cipher) for cipher in ciphers):
+        return 0
+    return sum(hunt(remainingcode[len(cipher):]) for cipher in ciphers if remainingcode.startswith(cipher))
+
+print(hunt(code))
+"""
